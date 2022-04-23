86bc077e9d87a66bc0cd23be854e75d97ed569ff
Robert Vári <mail.robertvari@gmail.com>
2022-04-23 14:36:16 +0200

    init

---
+++ 03_posittional_parameters.py
@@ -0,0 +1,8 @@
+def print_user_data(name, address, email, age):
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"Age: {age}")
+
+
+print_user_data("Robert", "Budapest", "robert@gmail.com", 30)
