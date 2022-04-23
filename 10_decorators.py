923aa674e4e77e534fde4f75bbda82bb6849b299
Robert Vári <mail.robertvari@gmail.com>
2022-04-23 16:42:15 +0200

    init

--- 10_decorators.py
+++ 10_decorators.py
@@ -16,18 +16,21 @@ def timer(func):
     return wrapper
 
 
+@timer
 def worker1():
     print("Worker1 Started")
     time.sleep(random.randint(3, 10))
     print("Worker1 Stopped.")
 
 
+@timer
 def worker2():
     print("Worker2 Started")
     time.sleep(random.randint(3, 10))
     print("Worker2 Stopped.")
 
 
+@timer
 def worker3():
     print("Worker3 Started")
     time.sleep(random.randint(3, 10))
