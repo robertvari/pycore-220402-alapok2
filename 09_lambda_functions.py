f995fa7373739420348b65ed3d1520ea5a6713c3
Robert Vári <mail.robertvari@gmail.com>
2022-04-23 16:02:09 +0200

    init

---
+++ 09_lambda_functions.py
@@ -0,0 +1,9 @@
+add_numbers = lambda a, b: a+b
+multiply_numbers = lambda a, b: a*b
+divide_numbers = lambda a, b: a/b
+
+add_result = add_numbers(10, 5)
+multiply_result = add_numbers(add_result, 5)
+divide_result = add_numbers(multiply_result, 5)
+
+name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
