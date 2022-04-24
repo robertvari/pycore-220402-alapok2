3911263d393a86758d245c72e233a47518a46958
Robert Vári <mail.robertvari@gmail.com>
2022-04-24 15:41:44 +0200

    update

--- TRexRunner/game_assets/start_screen.py
+++ TRexRunner/game_assets/start_screen.py
@@ -1,4 +1,13 @@
 import pygame
+from utilities.settings import GAMETITLE
+
+# setup game title
+title_font = pygame.font.Font("resources/dogicabold.ttf", 40)
+title = title_font.render(GAMETITLE, True, "white")
+
+# press space to start
+press_space_font = pygame.font.Font("resources/dogica.ttf", 25)
+press_space_text = press_space_font.render("Press Space to Start", True, "white")
 
 trex_image = pygame.image.load("resources/trex_idle.png")
 
@@ -10,3 +19,5 @@ def draw_start_screen(screen):
     :return: None
     """
     screen.blit(trex_image, (0, 0))
+    screen.blit(title, (0, 300))
+    screen.blit(press_space_text, (0, 150))
