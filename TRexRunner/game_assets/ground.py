fcfe7d323c3c8266d11da856059334dd71bb7ff8
Robert Vári <mail.robertvari@gmail.com>
2022-04-24 17:24:46 +0200

    update

--- TRexRunner/game_assets/ground.py
+++ TRexRunner/game_assets/ground.py
@@ -1,8 +1,11 @@
 import pygame
 from utilities.resources import get_resource
 
+ground_speed = 20
 ground_image = pygame.image.load(get_resource("ground.png"))
+ground_rect = ground_image.get_rect(bottomleft=(0, 400))
 
 
 def draw_ground(screen):
-    screen.blit(ground_image, (0, -30))
+    ground_rect.x -= ground_speed
+    screen.blit(ground_image, ground_rect)
