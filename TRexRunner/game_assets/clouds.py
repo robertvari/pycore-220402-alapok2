4659cef52f93dd3cca9bb3a56ec22900fdd61dca
Robert Vári <mail.robertvari@gmail.com>
2022-04-24 17:40:06 +0200

    update

--- TRexRunner/game_assets/clouds.py
+++ TRexRunner/game_assets/clouds.py
@@ -1,8 +1,24 @@
 import pygame
 from utilities.resources import get_resource
 
-cloud_image = pygame.image.load(get_resource("clouds.png"))
+cloud_speed = 5
+
+cloud_image1 = pygame.image.load(get_resource("clouds.png"))
+cloud_rect_1 = cloud_image1.get_rect(x=0, y=0)
+
+cloud_image2 = pygame.image.load(get_resource("clouds.png"))
+cloud_rect_2 = cloud_image1.get_rect(midleft=cloud_rect_1.midright)
 
 
 def draw_clouds(screen):
-    screen.blit(cloud_image, (0, 0))
+    if cloud_rect_1.right < 0:
+        cloud_rect_1.midleft = cloud_rect_2.midright
+
+    cloud_rect_1.x -= cloud_speed
+    screen.blit(cloud_image1, cloud_rect_1)
+
+    if cloud_rect_2.right < 0:
+        cloud_rect_2.midleft = cloud_rect_1.midright
+
+    cloud_rect_2.x -= cloud_speed
+    screen.blit(cloud_image2, cloud_rect_2)
