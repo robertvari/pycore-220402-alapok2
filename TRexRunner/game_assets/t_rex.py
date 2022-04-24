ddf9c88923b7e3731ed8496ddb120151906dd510
Robert Vári <mail.robertvari@gmail.com>
2022-04-24 17:20:10 +0200

    update

--- TRexRunner/game_assets/t_rex.py
+++ TRexRunner/game_assets/t_rex.py
@@ -3,9 +3,19 @@ from utilities.resources import get_resource
 
 ground_pos = 250
 
-trex_image = pygame.image.load(get_resource("trex_run_01.png"))
-trex_rect = trex_image.get_rect(x=50, y=ground_pos)
+trex_frames = [
+    pygame.image.load(get_resource("trex_run_01.png")),
+    pygame.image.load(get_resource("trex_run_02.png"))
+]
+frame_index = 0
+trex_rect = trex_frames[frame_index].get_rect(x=50, y=ground_pos)
 
 
 def draw_trex(screen):
-    screen.blit(trex_image, trex_rect)
+    global frame_index
+
+    frame_index += 0.3
+    if frame_index >= len(trex_frames):
+        frame_index = 0
+
+    screen.blit(trex_frames[int(frame_index)], trex_rect)
